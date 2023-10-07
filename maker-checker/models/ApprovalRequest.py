eb0194aaeb866fdf090eef8c2ee6e06f159df8ee
Xun Yi <xunyi.work@gmail.com>
2023-10-07 18:16:16 +0800

    Update and add in base model for updates&approve

--- maker-checker/models/ApprovalRequest.py
+++ maker-checker/models/ApprovalRequest.py
@@ -1,12 +1,15 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, Optional
 from uuid import uuid4
 from enum import Enum
 from typing import Dict
+from datetime import datetime, timedelta
 
 class ApprovalStatus(str, Enum):
     PENDING = "pending"
     APPROVED = "approved"
     REJECTED = "rejected"
+    WITHDRAWN = "withdrawn"
+    EXPIRED = "expired"
 
 class ApprovalRequest(BaseModel):
     uid: str = Field(default_factory=lambda: str(uuid4()))
@@ -14,3 +17,29 @@ class ApprovalRequest(BaseModel):
     request_type: str
     request_details: Dict[str, str]
     status: ApprovalStatus
+    created_at: str = datetime.now().isoformat()
+    comments: Optional[str] = None
+    request_title: Optional[str] = None
+    request_expiry: str = (datetime.now() + timedelta(days=5)).isoformat()
+    approval_role: str 
+
+class ApprovalUpdate(BaseModel):
+    """
+    Used when original requestor wants to make an update to the details.
+    Request cannot be updated once modified. 
+    """
+    status: Optional[ApprovalStatus]
+    comments: Optional[str]
+    request_details: Optional[Dict[str, str]]
+    request_title: Optional[str]
+    request_expiry: Optional[str]
+
+class ApprovalResponse(BaseModel):
+    """
+    Base model used when an approver rejects or approves a request.
+    """
+    status: ApprovalStatus
+    approver_id: int
+    comments: Optional[str]
+    resolution_at: str = datetime.now().isoformat()
+    
