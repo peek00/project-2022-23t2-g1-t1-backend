9d8212f7a509828bd74c4756fb23bfa5fc09c53f
Xun Yi <xunyi.work@gmail.com>
2023-10-12 14:55:55 +0800

    Added in get expired endpoint

--- maker-checker/views/approvals.py
+++ maker-checker/views/approvals.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from models.ApprovalRequest import ApprovalRequest, ApprovalUpdate, ApprovalResponse, DeleteRequest
 from models.approval_request_repository import ApprovalRequestRepository, ValidationError
 from controllers.db import initialize_db
-
+from botocore.exceptions import ClientError
 
 router = APIRouter(
   prefix = "/approval",
@@ -17,48 +17,99 @@ db = initialize_db()
 approval_request_repository = ApprovalRequestRepository(db)
 
 @router.get("/")
-async def home():
+async def healthcheck():
     return {
-        "message": "Welcome to the approvals API!",
+        "message": "Welcome to the approvals API! Endpoint is working.",
     }
+
+# =================== START: GET requests =======================
 @router.get("/get-all", response_model=None)
-async def get_all_approval_requests(
+async def get_all_requests(
 ):
-    return approval_request_repository.get_all_approval_requests()
+    try:
+        return approval_request_repository.get_all_approval_requests()
+    except ValidationError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except ClientError as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
     
+@router.get("/get-pending")
+def get_pending_requests():
+    try:
+        return approval_request_repository.get_pending_approval_requests()
+    except ValidationError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except ClientError as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@router.get("/get-approved")
+def get_approved_requests():
+    try:
+        return approval_request_repository.get_approved_approval_requests()
+    except ValidationError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except ClientError as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@router.get("/get-rejected")
+def get_rejected_requests():
+    try:
+        return approval_request_repository.get_rejected_approval_requests()
+    except ValidationError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except ClientError as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+@router.get("/get-expired")
+def get_expired_requests():
+    try:
+        return approval_request_repository.get_expired_approval_requests()
+    except ValidationError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except ClientError as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+# =================== END: GET requests =======================
+
+# =================== START: REQUESTOR requests =======================
 
 @router.post("/create", response_model=None)
 def create_approval_requests(
     data: ApprovalRequest,
 ):
-    if authorized_request():
-        return approval_request_repository.create_approval_request(data)
-
-@router.get("/get-pending")
-def get_pending_approval_requests():
-    return approval_request_repository.get_pending_approval_requests()
-
-@router.get("/get-approved")
-def get_pending_approval_requests():
-    return approval_request_repository.get_approved_approval_requests()
-
-@router.get("/get-rejected")
-def get_pending_approval_requests():
-    return approval_request_repository.get_rejected_approval_requests()
+    try:
+        if authorized_request():
+            return approval_request_repository.create_approval_request(data)
+    except ValidationError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except ClientError as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 @router.post("/update")
 def update_approval_request(
     data: ApprovalUpdate,
 ):
-    if authorized_request():
-        return approval_request_repository.update_approval_request(data)
-    
-@router.post("/response")
-def approve_approval_request(
-    data: ApprovalResponse,
-):
-    if authorized_request():
-        return approval_request_repository.approve_or_reject_approval_request(data)
+    try:
+        if authorized_request():
+            return approval_request_repository.update_approval_request(data)
+    except ValidationError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except ClientError as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
     
 @router.post("/withdraw")
 def withdraw_approval_request(
@@ -69,13 +120,41 @@ def withdraw_approval_request(
             return approval_request_repository.withdraw_approval_request(data)
     except ValidationError as e:
         raise HTTPException(status_code=400, detail=str(e))
-
-
+    except ClientError as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
 @router.post("/delete")
 def delete_approval_request(
     data: DeleteRequest,
 ):
-    if authorized_request():
-        return approval_request_repository.delete_approval_request(data)
-# ======== Approver Endpoints ====-====
-# @router.get("")
+    try:
+        if authorized_request():
+            return approval_request_repository.delete_approval_request(data)
+    except ValidationError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except ClientError as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+# =================== END: REQUESTOR requests =======================
+
+# =================== START: CHECKER requests =======================
+
+@router.post("/response")
+def approve_approval_request(
+    data: ApprovalResponse,
+):
+    try: 
+        if authorized_request():
+            return approval_request_repository.approve_or_reject_approval_request(data)
+    except ValidationError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except ClientError as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+# =================== END: CHECKER requests =======================
