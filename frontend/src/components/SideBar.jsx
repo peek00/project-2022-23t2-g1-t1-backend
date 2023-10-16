703a06972a4ebe53478c2ccb3dd9cc4795012151
Tan Yi Peng <yipeng.tan.2021@smu.edu.sg>
2023-10-16 13:13:46 +0800

    added routing for sign out and also sidebar

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -1,39 +1,47 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, NavLink, useLocation } from 'react-router-dom';
 
-export default function SideBar(){
+export default function SideBar() {
+  const [activeLink, setActiveLink] = useState('');
+  const location = useLocation();
 
+  // Check if the current location matches a link and update the active state
+  React.useEffect(() => {
+    setActiveLink(location.pathname);
+  }, [location]);
 
-    return(
+  return (
     <div className="min-h-screen w-[100%] z-50 relative">
+      <div>
+        <div className="flex flex-col items-center justify-center">
+          <Link to="/users"><img src="/footerLogo.svg" className="h-[100px] w-[100px]" /></Link>
 
-        <div>
+          <NavLink
+  to="/users"
+  activeClassName="active"
+  className={`text-white text-sm font-thin cursor-pointer ${activeLink === '/users' ? 'active opacity-100 font-bold' : 'opacity-60'}`}
+>
+  View Users
+</NavLink>
 
-            <div className="flex flex-col items-center justify-center">
 
-                <Link to="/users"><img src = "/footerLogo.svg" className="h-[100px] w-[100px]"/></Link>
+          <NavLink
+            to="/addUser"
+            activeClassName="active"
+            className={`text-white text-sm font-thin hover:font-bold cursor-pointer ${activeLink === '/addUser' ? 'opacity-100 font-bold' : 'opacity-60'}`}
+          >
+            Enroll User
+          </NavLink>
 
-                
-                <h1 className="text-white text-sm font-thin active:font-thick hover:font-bold cursor-pointer opacity-60 "><Link to="/users">View Users</Link></h1>
-                <h1 className="text-white text-sm font-thin active:font-thin hover:font-bold cursor-pointer opacity-60"><Link to="/addUser">Enroll User</Link></h1>
-                <h1 className="text-white text-sm font-thin active:font-thin hover:font-bold cursor-pointer opacity-60 mt-20"><Link to="/logs">Logs</Link></h1>
-                
-                </div>
-
+          <NavLink
+            to="/logs"
+            activeClassName="active"
+            className={`text-white  mt-20 text-sm font-thin hover:font-bold cursor-pointer ${activeLink === '/logs' ? 'opacity-100 font-bold' : 'opacity-60'}`}
+          >
+            Logs
+          </NavLink>
         </div>
-
-       
-
-
-
-
-
-
-
+      </div>
     </div>
-    )
-
-
-
-
+  );
 }
