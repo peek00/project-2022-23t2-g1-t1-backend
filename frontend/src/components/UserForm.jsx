2f65c31299e8d0fbbb6167b1725b045d5fb2d783
Tan Yi Peng <tanyipeng834@gmail.com>
2023-11-01 12:52:52 +0800

    attempt create user

--- frontend/src/components/UserForm.jsx
+++ frontend/src/components/UserForm.jsx
@@ -26,7 +26,7 @@ export default function UserForm(){
     <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Access/Role</label>
     
 <select id="countries" class="bg-gray-50 border px-5 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-  <option>Customer /User Admin</option>
+  <option>Customer</option>
   <option>Owner </option>
   <option>Manager</option>
   <option>Engineer</option>
