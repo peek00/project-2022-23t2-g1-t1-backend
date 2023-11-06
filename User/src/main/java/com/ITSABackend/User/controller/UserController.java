0649caa94f12cba39d70c2a7a576dbd8b99c4d44
JustinPansacola <pansacolajustin@gmail.com>
2023-11-06 14:00:09 +0800

    EmailFunct

--- User/src/main/java/com/ITSABackend/User/controller/UserController.java
+++ User/src/main/java/com/ITSABackend/User/controller/UserController.java
@@ -212,75 +212,52 @@ public class UserController {
         return new ResponseEntity<>(response, status);
     }
 
-    // @GetMapping(value = "/getUserEmailsByRole", produces = {"application/json"})
-    // public ResponseEntity<Map<String, Object>> getUserEmailsByRole(@PathParam("companyID") String companyID, @PathParam("roleName") String roleName) {
-    // Map<String, Object> response = new HashMap<>();
-    // HttpStatus status = HttpStatus.OK;
+    @GetMapping(value = "/getUserEmailsByRole", produces = {"application/json"})
+    public ResponseEntity<Map<String, Object>> getUserEmailsByRole(@RequestBody List<String> userIDs) {
+    Map<String, Object> response = new HashMap<>();
+    HttpStatus status = HttpStatus.OK;
 
-    //     try {
-    //         List<User> users = userService.getUsersByRoleFromCompany(companyID, roleName);
-    //         if (users.isEmpty()) {
-    //             throw new RuntimeException("No users found with the specified company / role");
-    //         }
+        try {
+            List<String> emails = userService.getUserEmailsFromCompany(userIDs);
+            if (emails.isEmpty()) {
+                throw new RuntimeException("No users found with the specified company / role");
+            }
+            
+            response.put("logInfo", "User emails from entered list retrieved successfully");
+            response.put("data", emails);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            response.put("logInfo", "Error occurred");
+            response.put("data", e.getMessage());
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
+        }
 
-    //         List<String> emails = users.stream().map(User::getEmail).collect(Collectors.toList());
+        return new ResponseEntity<>(response, status);
+    }
 
-    //         response.put("logInfo", "Users with role " + roleName + " retrieved successfully");
-    //         response.put("data", emails);
-    //     } catch (Exception e) {
-    //         System.err.println(e.getMessage());
-    //         response.put("logInfo", "Error occurred");
-    //         response.put("data", e.getMessage());
-    //         status = HttpStatus.INTERNAL_SERVER_ERROR;
-    //     }
+    @GetMapping(value = "/getUserEmails", produces = {"application/json"})
+    public ResponseEntity<Map<String, Object>> getUserEmailsByRole(@PathParam("roleName") String roleName, @RequestBody List<String> userIDs) {
+    Map<String, Object> response = new HashMap<>();
+    HttpStatus status = HttpStatus.OK;
 
-    //     return new ResponseEntity<>(response, status);
-    // }
+        try {
+            List<String> emails = userService.getUserEmailsFromCompanyByRole(userIDs, roleName);
+            if (emails.isEmpty()) {
+                throw new RuntimeException("No users found with the specified company / role");
+            }
+            
+            response.put("logInfo", "User emails from entered list retrieved successfully");
+            response.put("data", emails);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            response.put("logInfo", "Error occurred");
+            response.put("data", e.getMessage());
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
+        }
 
-    // @GetMapping(value="/getUsersByCompany", produces = {"application/json"})
-    // public ResponseEntity<Map<String, Object>> getUsersByCompany(@PathParam("companyID") String companyID){
-    //     Map<String, Object> response = new HashMap<>();
-    //     HttpStatus status = HttpStatus.OK;
+        return new ResponseEntity<>(response, status);
+    }
 
-
-    //     try{
-    //         List<User> users = userService.getUsersByCompany(companyID);
-    //         if (users.isEmpty()) {
-    //             throw new RuntimeException("No users found with the specified role");
-    //         }
-
-    //         response.put("logInfo", "Users from company" + companyID + " retrieved successfully");
-    //         response.put("data", users);
-    //     }catch(Exception e){
-    //         System.err.println(e.getMessage());
-    //         response.put("logInfo", "Error occurred");
-    //         response.put("data", e.getMessage());
-    //         status = HttpStatus.INTERNAL_SERVER_ERROR;
-
-    //     }
-    //     return new ResponseEntity<>(response, status);
-
-    // }
-
-    // @GetMapping(value = "/getUserEmailsByUserIDs", produces = {"application/json"})
-    // public ResponseEntity<Map<String, Object>> getUserEmailsByUserIDs(@PathParam("companyID") String companyID, @RequestBody List<String> userIDs) {
-    //     Map<String, Object> response = new HashMap<>();
-    //     HttpStatus status = HttpStatus.OK;
-
-    //     try {
-    //         List<String> emails = userService.getUserEmailsByUserIDsFromCompany(companyID, userIDs);
-
-    //         response.put("logInfo", "User emails retrieved successfully");
-    //         response.put("data", emails);
-    //     } catch (Exception e) {
-    //         System.err.println(e.getMessage());
-    //         response.put("logInfo", "Error occurred");
-    //         response.put("data", e.getMessage());
-    //         status = HttpStatus.INTERNAL_SERVER_ERROR;
-    //     }
-
-    //     return new ResponseEntity<>(response, status);
-    // }
 
 
 }
