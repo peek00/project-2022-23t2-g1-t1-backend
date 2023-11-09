5b0c240a573ec0b2435c835774f94a553446771a
Xun Yi <xunyi.work@gmail.com>
2023-11-10 03:35:42 +0800

    Frontend cleanup

--- frontend/src/components/maker_checker/MakerCheckerNav.jsx
+++ frontend/src/components/maker_checker/MakerCheckerNav.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 
+
 export default function MakerCheckerNav({ activeTab, onTabChange }) {
 
   const handleButtonClick = (state) => {
@@ -7,11 +8,12 @@ export default function MakerCheckerNav({ activeTab, onTabChange }) {
   };
 
   return (
-    <div className="top-[20%] left-[25%] w-[60%]">
+    <div className="top-[20%] left-[25%] w-[90%] mb-5">
       <div className="flex w-[50]align-bottom">
         <div className="mb-5 text-4xl">
           Approvals
         </div>
+
         <a
           href="/makerchecker/edit"
           className="float-right text-blue-500 ms-auto hover:underline"
@@ -21,7 +23,7 @@ export default function MakerCheckerNav({ activeTab, onTabChange }) {
         </a>
       </div>
 
-      <div>
+      <div className="flex">
         <button
           className={`me-5 btn ${activeTab === "pending" ? "active font-bold border-b-4 border-blue-500" : ""}`}
           onClick={() => handleButtonClick("pending")}
@@ -29,19 +31,20 @@ export default function MakerCheckerNav({ activeTab, onTabChange }) {
           Pending Approvals
         </button>
         <button
-          className={`mx-5 btn ${activeTab === "requested" ? "active font-bold border-b-4 border-blue-500" : ""}`}
+          className={`mx-5 btn ${activeTab === "requested" ? "active font-bold border-b-4 border-green-500" : ""}`}
           onClick={() => handleButtonClick("requested")}
         >
           My Request
         </button>
         <button
-          className={`mx-5 btn ${activeTab === "history" ? "active font-bold border-b-4 border-blue-500" : ""}`}
+          className={`mx-5 btn ${activeTab === "history" ? "active font-bold border-b-4 border-red-500" : ""}`}
           onClick={() => handleButtonClick("history")}
         >
           History
         </button>
         <button
-          className="float-right px-4 py-2 text-white bg-blue-500 rounded"
+          style={{ verticalAlign: "baseline" }}
+          className="float-right px-4 py-2 text-white align-bottom bg-blue-500 rounded ms-auto"
           onClick={() => handleButtonClick("create")}
         >
           Create Request
--- frontend/src/components/maker_checker/PendingApprovalTable.jsx
+++ frontend/src/components/maker_checker/PendingApprovalTable.jsx
@@ -12,10 +12,7 @@ export default function ApprovalTable({ data, activeTab, selectedCompany }) {
         }
     }, [data, activeTab]);
 
-    if (!shownData || shownData.length === 0) {
-        return <div className="ms-[40px]">No data available.</div>;
-    }
-    // @ChatGPT 
+    // @ChatGPT
     function getRelativeTimeOrExpired(timestamp) {
         const now = new Date();
         const timestampDate = new Date(timestamp);
@@ -23,17 +20,23 @@ export default function ApprovalTable({ data, activeTab, selectedCompany }) {
         const timeDifference = timestampDate - now;
 
         const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+        const hours = Math.floor(
+            (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        );
+        const minutes = Math.floor(
+            (timeDifference % (1000 * 60 * 60)) / (1000 * 60)
+        );
 
         if (timeDifference < 0) {
             return false;
         } else if (days > 0) {
-            return `${days} day${days > 1 ? 's' : ''}, ${hours} hr${hours > 1 ? 's' : ''}`;
+            return `${days} day${days > 1 ? "s" : ""}, ${hours} hr${hours > 1 ? "s" : ""
+                }`;
         } else if (hours > 0) {
-            return `${hours} hr${hours > 1 ? 's' : ''}, ${minutes} min${minutes > 1 ? 's' : ''}`;
+            return `${hours} hr${hours > 1 ? "s" : ""}, ${minutes} min${minutes > 1 ? "s" : ""
+                }`;
         } else {
-            return `${minutes} min${minutes > 1 ? 's' : ''}`;
+            return `${minutes} min${minutes > 1 ? "s" : ""}`;
         }
     }
 
@@ -86,27 +89,61 @@ export default function ApprovalTable({ data, activeTab, selectedCompany }) {
             console.error("Error approving:", error);
         }
     };
+
+    if (!shownData || shownData.length === 0) {
+        return (
+            <div>
+                <table className="min-w-full">
+                    <thead>
+                        <tr>
+                            <th className="w-1/6 py-3 text-xs font-bold tracking-wider text-left uppercase text-black-500">
+                                Request Type
+                            </th>
+                            <th className="w-1/6 py-3 text-xs font-bold tracking-wider text-left uppercase text-black-500 ">
+                                Request Details
+                            </th>
+                            <th className="w-1/6 py-3 text-xs font-bold tracking-wider text-left uppercase text-black-500">
+                                Comments
+                            </th>
+                            <th className="w-1/6 py-3 text-xs font-bold tracking-wider text-left uppercase truncate text-black-500">
+                                Requested By
+                            </th>
+                            <th className="w-1/6 py-3 text-xs font-bold tracking-wider text-left uppercase text-black-500 ">
+                                Expiry
+                            </th>
+                            <th className="w-1/6 py-3 text-xs font-bold tracking-wider text-left uppercase text-black-500">
+                                Actions
+                            </th>
+                        </tr>
+                    </thead>
+                </table>
+                <div className="grid text-2xl text-center text-gray-300 place-items-center" style={{ height: "500px" }}>
+                    No pending requests.
+                </div>
+            </div>
+        );
+    }
     return (
-        <div className="ms-[40px]">
+        <div >
             <table className="min-w-full">
                 <thead>
                     <tr>
-                        <th className="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">
+                        <th className="px-6 py-3 text-xs font-bold tracking-wider text-left uppercase text-black-500">
                             Request Type
                         </th>
-                        <th className="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">
+                        <th className="px-6 py-3 text-xs font-bold tracking-wider text-left uppercase text-black-500">
                             Request Details
                         </th>
-                        <th className="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">
+                        <th className="px-6 py-3 text-xs font-bold tracking-wider text-left uppercase text-black-500">
                             Comments
                         </th>
-                        <th className="w-6 px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase truncate">
+                        <th className="w-6 px-6 py-3 text-xs font-bold tracking-wider text-left uppercase truncate text-black-500">
                             Requested By
                         </th>
-                        <th className="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase ">
+                        <th className="px-6 py-3 text-xs font-bold tracking-wider text-left uppercase text-black-500 ">
                             Expiry
                         </th>
-                        <th className="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">
+                        <th className="px-6 py-3 text-xs font-bold tracking-wider text-left uppercase text-black-500">
                             Actions
                         </th>
                     </tr>
@@ -130,14 +167,14 @@ export default function ApprovalTable({ data, activeTab, selectedCompany }) {
                                 {request.requestor_id || "N/A"}
                             </td>
                             <td className="px-6 py-4 whitespace-nowrap">
-                                {
-                                    getRelativeTimeOrExpired(request.request_expiry) === false
-                                        ? "Expired"
-                                        : getRelativeTimeOrExpired(request.request_expiry)
-                                }                            </td>
+                                {getRelativeTimeOrExpired(request.request_expiry) === false
+                                    ? "Expired"
+                                    : getRelativeTimeOrExpired(request.request_expiry)}{" "}
+                            </td>
                             <td className="px-6 py-4 whitespace-nowrap">
                                 <div>
-                                    {getRelativeTimeOrExpired(request.request_expiry) === false ? (
+                                    {getRelativeTimeOrExpired(request.request_expiry) ===
+                                        false ? (
                                         <div>
                                             <button
                                                 className="px-4 py-2 mr-2 text-white bg-gray-400 rounded cursor-not-allowed"
@@ -162,7 +199,6 @@ export default function ApprovalTable({ data, activeTab, selectedCompany }) {
                                             </button>
                                         </div>
                                     )}
-
                                 </div>
                             </td>
                         </tr>
