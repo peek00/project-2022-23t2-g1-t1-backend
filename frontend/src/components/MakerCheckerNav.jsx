1514edb947f8c313847a6b2653934907bd05d92c
Xun Yi <xunyi.work@gmail.com>
2023-11-09 22:36:44 +0800

    Made makerchecker 60% complete bruh

---
+++ frontend/src/components/MakerCheckerNav.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+export default function MakerCheckerNav({ activeTab, onTabChange }) {
+
+  const handleButtonClick = (state) => {
+    onTabChange(state);
+};
+
+  return (
+    <div className="top-[20%] left-[25%] w-[60%]">
+      <div className="mb-5 text-4xl">Approvals</div>
+      <button
+        className={`me-5 btn ${activeTab === "pending" ? "active font-bold border-b-4 border-blue-500" : ""}`}
+        onClick={() => handleButtonClick("pending")}
+      >
+        Pending Approvals
+      </button>
+      <button
+        className={`mx-5 btn ${activeTab === "requested" ? "active font-bold border-b-4 border-blue-500" : ""}`}
+        onClick={() => handleButtonClick("requested")}
+      >
+        My Request
+      </button>
+      <button
+        className={`mx-5 btn ${activeTab === "history" ? "active font-bold border-b-4 border-blue-500" : ""}`}
+        onClick={() => handleButtonClick("history")}
+      >
+        History
+      </button>
+      <button 
+      className="float-right px-4 py-2 text-white bg-blue-500 rounded"
+      onClick={() => handleButtonClick("create")}
+      >
+        Create Request
+      </button>
+    </div>
+  );
+}
