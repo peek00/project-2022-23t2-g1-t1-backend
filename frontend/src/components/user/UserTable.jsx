774e3ad9caba9ec8b06b840924f6580b744dfe7c
Tan Yi Peng <tanyipeng834@gmail.com>
2023-11-10 18:03:23 +0800

    added pagination

--- frontend/src/components/user/UserTable.jsx
+++ frontend/src/components/user/UserTable.jsx
@@ -6,6 +6,7 @@ import {API_BASE_URL} from "@/config/config";
 export default function UserTable() {
     const [users, setUsers] = useState([]);
     const [role, setRole] = useState(null);
+    const [currentPage, setCurrentPage] = useState(1);
 
     useEffect(() => {
       viewUser()
@@ -34,6 +35,17 @@ export default function UserTable() {
         console.error("Cannot view user:", error);
       }
     }
+    const handlePreviousPage = () => {
+      if (currentPage > 1) {
+        setCurrentPage((prevPage) => prevPage - 1);
+      }
+    };
+  
+    const handleNextPage = () => {
+    
+        setCurrentPage((prevPage) => prevPage + 1);
+      
+    };
 
   return (
     <div className="relative overflow-x-auto w-[85%] mb-[100px]">
@@ -83,6 +95,22 @@ export default function UserTable() {
           ))}
         </tbody>
       </table>
+      <div className="flex justify-end p-4">
+        <button
+          className="bg-blue-500 text-white px-4 py-2 mx-4"
+          onClick={handlePreviousPage}
+        >
+          Previous
+        </button>
+        Current Page : {currentPage}
+        <button
+          className="bg-blue-500 text-white px-4 py-2 mx-4"
+          onClick={handleNextPage}
+        >
+          Next
+        </button>
+      </div>
     </div>
+  
   );
 }
