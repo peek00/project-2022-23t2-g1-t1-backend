0f3bed9ce5d0cb6d6006a9837d0eaeaf46878914
bryanleezh <leebryan307@gmail.com>
2023-11-11 22:55:15 +0800

    fixed issues with getting the data from userside

--- frontend/src/components/account/UserAccountTable.jsx
+++ frontend/src/components/account/UserAccountTable.jsx
@@ -7,22 +7,22 @@ import { getAllUserAccountsByCompanyId } from '../../apis/points';
 
 export default function UserAccountTable({ companyId }){
     const [accounts, setAccounts] = useState([]);
-    console.log(companyId);
-
+    // console.log(companyId);
     // company and points
 
     useEffect(() => {
         getAllUserAccountsByCompanyId(companyId)
           .then(async (results) => {
             const accountsData = results.data;
-    
+            // console.log(accountsData);
             // Fetch additional data for each account
             const accountsWithAdditionalData = await Promise.all(
               accountsData.map(async (account) => {
                 try {
-                  const response = await axios.get(API_BASE_URL + `User/getUser?userID=` + account.userId, {
+                  const response = await axios.get(API_BASE_URL + `User/getUser?userID=` + account["user_id"], {
                     withCredentials: true
                   });
+                  // const response = await axios.get("http://localhost:8080/User/getUser?userID=" + account["user_id"]);
                   // Merge the additional data with the account data
                   return {...account, userData: response.data.data };
                 } catch (error) {
@@ -31,8 +31,11 @@ export default function UserAccountTable({ companyId }){
                 }
               })
             );
-    
-            setAccounts(accountsWithAdditionalData);
+            const newdata = accountsWithAdditionalData;
+            console.log(newdata);
+            setAccounts(newdata);
+            // console.log(accountsData)
+            // setAccounts(accountsData)
           })
           .catch((error) => {
             console.error('Error fetching accounts:', error);
@@ -45,8 +48,10 @@ export default function UserAccountTable({ companyId }){
         //   return err;
         // })
         // console.log(accounts);
-      }, [companyId]);
-  
+      }, []);
+      useEffect(() => {
+        console.log('Accounts updated:', accounts);
+      }, [accounts]);
     return (
       <div className="relative overflow-x-auto w-[85%] mb-[100px]">
         <table className="w-full text-sm text-left bg-[#F5F5F5]">
@@ -73,7 +78,14 @@ export default function UserAccountTable({ companyId }){
                 <td className="px-20 py-4">{account.userData.email}</td>
                 <td className="px-6 py-4">{account.balance}</td>               
                 <td>
-                <EditAccount companyId={account.company_id} pointsId ={account.user_id} points={account.balance} />
+                <ListItemSuffix>
+                  <Link to={`/user/account/company/${companyId}/${account['user_id']}/editPoints`}>
+                    <IconButton variant="text" color="blue-gray">
+                      <NextIcon />
+                    </IconButton>
+                  </Link>
+                </ListItemSuffix>
+                {/* <EditAccount companyId={account.company_id} pointsId ={account.user_id} points={account.balance} /> */}
                 </td>
               </tr>
             ))}
