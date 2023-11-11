f82a5ca770d45530f1044d728b765a949d2938fe
Tan Yi Peng <tanyipeng834@gmail.com>
2023-11-11 20:46:06 +0800

    done with user pagination with prefetching

--- frontend/src/components/user/UserTable.jsx
+++ frontend/src/components/user/UserTable.jsx
@@ -1,51 +1,126 @@
-import { useEffect, useState,useContext } from "react";
-import MenuDefault from "../common_utils/MenuDefault.jsx";
-import axios from "axios";
-import {API_BASE_URL} from "@/config/config";
+import { useEffect, useState, useRef } from 'react';
+import axios from 'axios';
+import { API_BASE_URL } from '@/config/config';
+import MenuDefault from '../common_utils/MenuDefault';
 
 export default function UserTable() {
-    const [users, setUsers] = useState([]);
-    const [role, setRole] = useState(null);
-    const [currentPage, setCurrentPage] = useState(1);
+  const [users, setUsers] = useState([]);
+  const [role, setRole] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const nextPage = useRef(null);
+  const previousPage = useRef(null);
+  const lastEvaluatedKey = useRef(null);
+  const viewAdmin = useRef(null);
+  const [viewUserFinished, setViewUserFinished] = useState(false);
+  const hasNext = useRef(true);
 
-    useEffect(() => {
-      viewUser()
-      // Fetch the role from localStorage or an API here
-      const storedRole = JSON.parse(localStorage.getItem("permissions"));
-      console.log(storedRole);
-      setRole(storedRole);
-      
-    }, []);
-
-    const viewUser = async () => {
-      // First check user permissions to access ?isAdmin=True
-      try {
-        const viewUserPermissions = await axios.post(API_BASE_URL+"/policy/permissions", {
-          pageLs: ["user/User/getAllUsers?isAdmin=True"],
-        }, {
-          withCredentials: true
-        });
-        const canViewAdmin = viewUserPermissions.data["user/User/getAllUsers?isAdmin=True"].GET;
-        const response = await axios.get(API_BASE_URL+`/api/user/User/getAllUsers?isAdmin=${canViewAdmin}`, {
-          withCredentials: true
-        })
-        setUsers(response.data.data);
-      } catch (error) {
-        // Handle errors here
-        console.error("Cannot view user:", error);
-      }
-    }
-    const handlePreviousPage = () => {
-      if (currentPage > 1) {
-        setCurrentPage((prevPage) => prevPage - 1);
-      }
-    };
-  
-    const handleNextPage = () => {
+  useEffect(() => {
+    viewUser();
     
-        setCurrentPage((prevPage) => prevPage + 1);
+    // Fetch the role from localStorage or an API here
+    const storedRole = JSON.parse(localStorage.getItem('permissions'));
+    console.log(storedRole);
+    setRole(storedRole);
+  }, []);
+
+  useEffect(() => {
+    // Prefetch the next page data only when viewUser has finished or the component has mounted
+    if (viewUserFinished||currentPage) {
+      prefetchNextPageData();
+    }
+  }, [viewUserFinished,currentPage]);
+
+  const viewUser = async () => {
+    try {
+      const viewUserPermissions = await axios.post(
+        API_BASE_URL + '/policy/permissions',
+        {
+          pageLs: ['user/User/getAllUsers?isAdmin=True'],
+        },
+        {
+          withCredentials: true,
+        }
+      );
+      const canViewAdmin =
+        viewUserPermissions.data['user/User/getAllUsers?isAdmin=True'].GET;
+      viewAdmin.current = canViewAdmin;
+
+      console.log(lastEvaluatedKey.current);
+
+      let response;
+      if (lastEvaluatedKey.current === null) {
+        response = await axios.get(
+        
+          API_BASE_URL+ `/api/user/User/getAllUsersPaged?isAdmin=${canViewAdmin}` ,
+          {
+            withCredentials: true,
+          }
+        );
+        console.log(response)
+        setUsers(response.data.data.users);
+      lastEvaluatedKey.current = response.data.data.newLastEvaluatedKey;
+
+      if(response.data.data.next){
+        hasNext.current = true;
+      }
+      else{
+        hasNext.current = false;
+      }
+      } 
       
-    };
+      
+      setViewUserFinished(true); // Indicate that viewUser has finished
+    } catch (error) {
+      console.error('Cannot view user:', error);
+    }
+  };
+
+  const handlePreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage((prevPage) => prevPage - 1);
+    }
+    setUsers(previousPage.current);
+  };
+
+  const handleNextPage = () => {
+    previousPage.current = users;
+    if(lastEvaluatedKey.current != null){
+    setCurrentPage((prevPage) => prevPage + 1);
+    setUsers(nextPage.current);
+  }
+  else{
+    alert("No more pages to show")
+  }
+};
+
+  const prefetchNextPageData = async () => {
+
+    if(lastEvaluatedKey!=null){
+    try {
+      const response = await axios.get(
+      
+        API_BASE_URL+ `/api/user/User/getAllUsersPaged?isAdmin=${viewAdmin.current}&lastEvaluatedKey=${lastEvaluatedKey.current}`,
+        {
+          withCredentials: true,
+        }
+      );
+
+      // You can use the prefetched data as needed, for example, update state or store it in a ref.
+      nextPage.current = response.data.data.users;
+      hasNext.current = response.data.data.next;
+      console.log(hasNext.current);
+      lastEvaluatedKey.current = response.data.data.newLastEvaluatedKey;
+      console.log(nextPage.current);
+      console.log(response)
+    } catch (error) {
+      console.error('Error prefetching data:', error);
+    }
+  };
+};
+
+  // Rest of your component code...
+
+
 
   return (
     <div className="relative overflow-x-auto w-[85%] mb-[100px]">
@@ -113,4 +188,6 @@ export default function UserTable() {
     </div>
   
   );
+
+
 }
