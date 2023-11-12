799079957cf78bcf7f01af2b223bb1342f6857f7
Tan Yi Peng <tanyipeng834@gmail.com>
2023-11-12 12:01:36 +0800

    added a loader to the frontend

--- frontend/src/router/PrivateRoute.jsx
+++ frontend/src/router/PrivateRoute.jsx
@@ -2,42 +2,59 @@ import React, { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import AlertIcon from '../components/common_utils/AlertIcon';
 import axios from 'axios';
-import {API_BASE_URL} from "@/config/config";
+import { API_BASE_URL } from '@/config/config';
 import ErrorPage from '../pages/errorpage';
+import { Spinner } from "@material-tailwind/react";
 
-const PrivateRoute = ({ page,permission }) => {
+// Add a loader component
+
+
+const PrivateRoute = ({ page, permission }) => {
   const [authorized, setAuthorized] = useState(null);
+  const [loading, setLoading] = useState(true); // Add loading state
 
   useEffect(() => {
     const fetchRole = async () => {
-      const body = { "pageLs": ["user", "points", "maker-checker", "policy", "logging","user/User/getAllUsersPaged?isAdmin=True","user/User/getAllUsersPaged?isAdmin=False"]};
+      const body = {
+        pageLs: [
+          'user',
+          'points',
+          'maker-checker',
+          'policy',
+          'logging',
+          'user/User/getAllUsersPaged?isAdmin=True',
+          'user/User/getAllUsersPaged?isAdmin=False',
+        ],
+      };
       try {
-        const response = await axios.post(API_BASE_URL+'/policy/permissions', body, {
-          withCredentials: true
+        const response = await axios.post(API_BASE_URL + '/policy/permissions', body, {
+          withCredentials: true,
         });
 
         console.log(response.data);
 
-        const roleresponse = await axios.get(API_BASE_URL+"/auth/me", {
-          withCredentials: true
+        const roleresponse = await axios.get(API_BASE_URL + '/auth/me', {
+          withCredentials: true,
         });
 
-        const userDetailsResponse = await axios.get(API_BASE_URL+"/api/user/User/getUser?userID=" + roleresponse.data.id, {
-          withCredentials: true
-        });
-        
+        const userDetailsResponse = await axios.get(
+          API_BASE_URL + '/api/user/User/getUser?userID=' + roleresponse.data.id,
+          {
+            withCredentials: true,
+          }
+        );
 
-        localStorage.setItem("role", JSON.stringify(roleresponse.data.role));
-        localStorage.setItem("id", JSON.stringify(roleresponse.data.id));
-        //Set the user name 
-        localStorage.setItem("UserName",JSON.stringify(userDetailsResponse.data.data.fullName))
+        localStorage.setItem('role', JSON.stringify(roleresponse.data.role));
+        localStorage.setItem('id', JSON.stringify(roleresponse.data.id));
+        //Set the user name
+        localStorage.setItem('UserName', JSON.stringify(userDetailsResponse.data.data.fullName));
         // Assuming the response contains the user's role
-        localStorage.setItem("permissions", JSON.stringify(response.data));
-        console.log(localStorage.getItem("permissions"));
+        localStorage.setItem('permissions', JSON.stringify(response.data));
+        console.log(localStorage.getItem('permissions'));
         return response.data; // This is the role
       } catch (error) {
         // Handle errors here
-        console.error("Error fetching role:", error);
+        console.error('Error fetching role:', error);
         throw error;
       }
     };
@@ -46,24 +63,19 @@ const PrivateRoute = ({ page,permission }) => {
       .then((role) => {
         // Check if the user is authorized to access the page
         console.log(role);
-        
-        if(page =='user'&& permission == 'GET'){
-          const isAdmin = role["user/User/getAllUsersPaged?isAdmin=True"]['GET'];
+
+        if (page == 'user' && permission == 'GET') {
+          const isAdmin = role['user/User/getAllUsersPaged?isAdmin=True']['GET'];
           console.log(isAdmin);
-          const isUser = role["user/User/getAllUsersPaged?isAdmin=False"]['GET'];
+          const isUser = role['user/User/getAllUsersPaged?isAdmin=False']['GET'];
           console.log(isUser);
 
-          if(isAdmin || isUser){
+          if (isAdmin || isUser) {
             setAuthorized(true);
-          }
-          else{
+          } else {
             setAuthorized(false);
           }
-
-
-
-        }
-        else if (role[page][permission]) {
+        } else if (role[page][permission]) {
           setAuthorized(true);
         } else {
           setAuthorized(false);
@@ -71,12 +83,19 @@ const PrivateRoute = ({ page,permission }) => {
       })
       .catch((error) => {
         // Handle errors
-        console.error("Authorization check error:", error);
+        console.error('Authorization check error:', error);
         setAuthorized(false);
+      })
+      .finally(() => {
+        // Set loading to false when the fetch is complete
+        setLoading(false);
       });
   }, [page]);
 
-  if (authorized === null) {
+  // Render loader if still loading
+  if (loading) {
+    return <Spinner className="h-10 w-10" />;
+  } else if (authorized === null) {
     return null; // You can render a loading spinner or something here
   } else if (authorized) {
     return <Outlet />;
