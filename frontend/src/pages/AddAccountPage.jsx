25a9ee5628c0c3111d0cec85dbf1c76b0c761a29
bryanleezh <leebryan307@gmail.com>
2023-11-12 22:29:56 +0800

    fixed url links + addAccountPage integration

--- frontend/src/pages/AddAccountPage.jsx
+++ frontend/src/pages/AddAccountPage.jsx
@@ -4,8 +4,11 @@ import SideBar from '../components/common_utils/SideBar';
 import TopBar from '../components/common_utils/TopBar';
 
 import AddAccountForm from '../components/account/AddAccountForm';
+import { useParams } from 'react-router-dom';
 
 export default function AddAccountPage() {
+  const { userId } = useParams();
+
   const goBack = () => {
     window.history.back(); // This will take the user back one step in the browser's history.
   };
@@ -32,7 +35,7 @@ export default function AddAccountPage() {
 
             <div className='absolute left-[20%] min-w-[65%]'>
 
-              <AddAccountForm />
+              <AddAccountForm userId={userId}/>
               <h1 className='text-3xl font-bold mt-[20%]'>
                 Add Account
               </h1>
