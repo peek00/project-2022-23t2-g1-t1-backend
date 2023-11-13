d32000e92596a01ca48bb58abadfb5b0e0a85c61
Xun Yi <xunyi.work@gmail.com>
2023-11-14 00:49:20 +0800

    Added in feedback for approving requests

--- frontend/src/components/maker_checker/ApprovalButton.jsx
+++ frontend/src/components/maker_checker/ApprovalButton.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import CircularProgress from '@mui/material/CircularProgress';
 
-export default function ApprovalButton({ handleApprove, handleReject }) {
+export default function ApprovalButton({ handleApprove, handleReject, submissionState }) {
     const [action, setAction] = useState(null);
     const [loading, setLoading] = useState(false);
 
@@ -9,7 +9,6 @@ export default function ApprovalButton({ handleApprove, handleReject }) {
         setAction(action);
         setLoading(true);
         try {
-
             if (action == "approve") {
                 handleApprove();
             }
@@ -32,29 +31,31 @@ export default function ApprovalButton({ handleApprove, handleReject }) {
             <div>
                 <button
                     className="px-4 py-2 mr-2 text-white bg-green-500 rounded"
-                    onClick={() => { handleClick("approve") }}
+                    onClick={() => handleClick("approve")}
                 >
                     Approve
                 </button>
                 <button
                     className="px-4 py-2 text-white bg-red-500 rounded"
-                    onClick={() => { handleClick("reject") }}
+                    onClick={() => handleClick("reject")}
                 >
                     Reject
                 </button>
             </div>
         );
-    }
-    else {
+    } else {
         return (
             <div>
-                {loading ? <CircularProgress /> :
-                    <p className="font-bold text-green-500">
-                        Resolved
-                    </p>
-                }
+                {!loading && submissionState !== "error" ? (  // If finish loading, and not error 
+                    submissionState === "accepted" || submissionState === "rejected" ? (
+                        <p className="font-bold text-green-500">{submissionState}</p>
+                    ) : (
+                        <p className="font-bold text-red-500">Error!</p>
+                    )
+                ) : (
+                    loading && <CircularProgress />
+                )}
             </div>
-
-        )
+        );
     }
 }
