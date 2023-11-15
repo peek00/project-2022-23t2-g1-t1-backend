3378784c7267edb7e559ebd073d51a767ac0a2d6
Ng Shen Jie <ng.shen.jie1999@gmail.com>
2023-11-15 21:25:52 +0800

    fix: update to logging MS to include delete all old files endpoints

--- logging/logRoutes.js
+++ logging/logRoutes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import * as logController from "./logController.js";
+import * as fileController from "./processFileController.js";
 
 const router = Router();
 
 router.get("/", logController.getAllLogs);
 router.get("/group", logController.getAllLogGroups);
+router.get("/clear", fileController.clearAllLogs);
 
 export default router;
